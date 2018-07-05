e04a7c8adfec859503159a429a6a18da2c641233
CIPL0499 <CIPL0499@CIPl0499JAVA.colanonline.local>
2018-07-05 19:38:01 +0530

    main sysout3 commit

--- src/main/java/com/explore/main/Main.java
+++ src/main/java/com/explore/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("welcome to jenkins");
 		System.out.println("welcome to jenkins");
+		System.out.println("welcome to jenkins");
 	}
 
 }

a242507e1e77977bca366ff0cc2815fa3d0f1cfd
CIPL0499 <CIPL0499@CIPl0499JAVA.colanonline.local>
2018-07-06 12:09:32 +0530

    c commit

--- src/main/java/com/explore/main/Main.java
+++ src/main/java/com/explore/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("welcome to jenkins");
 		System.out.println("welcome to jenkins");
 		
-		int a=10, b=20;
+		int a=10, b=20, c=0;
 		System.out.println("addition ::"+(a+b));
 		System.out.println("welcome to jenkins");
 	}

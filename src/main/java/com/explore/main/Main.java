849122bd292bcf46c05f8acd8cdccbbdee890e0f
CIPL0499 <CIPL0499@CIPl0499JAVA.colanonline.local>
2018-07-06 11:43:43 +0530

    addition commit

--- src/main/java/com/explore/main/Main.java
+++ src/main/java/com/explore/main/Main.java
@@ -6,13 +6,10 @@ public class Main {
 		System.out.println("welcome to jenkins");
 		System.out.println("welcome to jenkins");
 		System.out.println("welcome to jenkins");
-<<<<<<< HEAD
 		
 		int a=10, b=20;
 		System.out.println("addition ::"+(a+b));
-=======
 		System.out.println("welcome to jenkins");
->>>>>>> 1ff800d9325e4bbd671dcffdf9fdfa8a90a9118a
 	}
 
 }
